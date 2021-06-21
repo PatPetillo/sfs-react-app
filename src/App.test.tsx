557e802b71005c18d505a4e3f017c8367ce708d3
Pat <pat@pats-mbp.myfiosgateway.com>
2021-06-21 12:40:26 -0400

    Working application

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the App', () => {
   const app = render(<App />);
   expect(app).toBeTruthy();
 });
